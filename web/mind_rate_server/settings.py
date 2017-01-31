7e5d9b2b4c1ae25de0b8f196cb050f28dbb8b5de
Shanshan Du <du.shanshan@outlook.com>
2017-01-31 20:17:04 +0100

    changed dir

--- web/mind_rate_server/settings.py
+++ web/mind_rate_server/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mind_rate_server.survey',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
---
+++ web/mind_rate_server/survey/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Questionnaire(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Question(models.Model):
+    questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
