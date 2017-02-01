ef1c240491616196c575d982bb8f9ce2f59b4533
Yi Ge <me@yige.ch>
2017-02-01 18:00:05 +0100

    fix static dir

--- web/mind_rate_server/settings.py
+++ web/mind_rate_server/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static_media/')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# print(STATICFILES_DIRS)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
