6ba9f6af4760e7687736c7f519eb6023f444ecd6
Yi Ge <me@yige.ch>
2017-02-14 15:30:18 +0100

    give user all permissions of the survey app

--- web/mind_rate_server/survey/views.py
+++ web/mind_rate_server/survey/views.py
@@ -2,20 +2,30 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views import generic
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
 
 from .models import Study
 
 
 @login_required(login_url="login/")
 def home(request):
-
-    # give user permission to access the admin site
     user = User.objects.get(username=request.user.username)
+
+    # give user access to the admin site
     user.is_staff = True
+
+    # all permissions of the survey app
+    content_type_list = ContentType.objects.filter(app_label='survey')
+
+    for content_type in content_type_list:
+        user.user_permissions.add(
+            *Permission.objects.filter(content_type__exact=content_type)
+        )
+
     user.save()
 
-    # send user to admin site
+    # send user to the admin site
     return redirect('/admin/')
 
 
