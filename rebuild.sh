e989181d1604f349b0ef65e7361e1e864d22f789
Yi Ge <me@yige.ch>
2017-03-20 18:11:40 +0100

    use python3 in setup script

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
-python web/manage.py makemigrations survey
-python web/manage.py migrate
+python3 web/manage.py makemigrations survey
+python3 web/manage.py migrate
 sudo docker-compose down
 sudo docker-compose build --no-cache
 sudo docker-compose up -d
